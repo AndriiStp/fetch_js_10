6eb3ec478288ef8fb2d0db47f098c98d37821e8a
Andrii Stepanenko <andrii.stp@gmail.com>
2024-03-28 17:39:57 +0200

    сми

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
 const breeds = document.querySelector('breed-select');
+const loadingStatus = document.querySelector('loader');
+const errorStatus = document.querySelector('error');
+const catInfo = document.querySelector('cat-info');
+
+breeds.addEventListener('select', onSelect);
+
+const onSelect = e => {
+  console.log(e.currentTarget.value);
+};
 
 axios.defaults.headers.common['x-api-key'] =
   'live_BOUcQBkQU24naJdyeEEfzWnFXCLg2oCSBdK5XaozfAkbX7IxnzAryZW6TQiQjTfI';
