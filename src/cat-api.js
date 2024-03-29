248efbc8533cf67a8fafdd7d1bf2c674ebc5e376
Andrii Stepanenko <andrii.stp@gmail.com>
2024-03-29 16:20:51 +0200

    add list render

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,24 +3,24 @@ const ENDPOINT = 'breeds';
 const API_KEY =
   'live_2U6Aps8M3yLcjxELTfcld9bGbmAM6bxwMoJPAZNXqA4x1mtu31dzKNnOoZ0QMiWe';
 
-const fetchBreeds = async (BASE_URL, API_KEY) => {
+function fetchBreeds() {
   const URL = `${BASE_URL}${ENDPOINT}?api_key=${API_KEY}`;
-  const resp = await fetch(URL);
-  if (!resp.ok) {
-    throw new Error(resp.statusText);
-  }
-  return await resp.json();
-};
+  return fetch(URL).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
 
-const fetchCatByBreed = async breedId => {
-  const BREED_URL = `${BASE_URL}/images/search&api_key=${API_KEY}&breed_ids=${breedId}`;
-  const resp = await fetch(BREED_URL);
-  if (!resp.ok) {
-    throw new Error(resp.statusText);
-  }
-  return await resp.json();
-};
-
-console.log(fetchBreeds(BASE_URL, API_KEY));
+function fetchCatByBreed(breedId) {
+  const BREED_URL = `${BASE_URL}images/search?api_key=${API_KEY}&breed_ids=${breedId}`;
+  return fetch(BREED_URL).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
 
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import axios from 'axios';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const breeds = document.querySelector('.breed-select');
+const breedsSelect = document.querySelector('.breed-select');
 const loadingStatus = document.querySelector('.loader');
 const errorStatus = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 
-const onSelect = e => {
-  console.log(e.currentTarget.value);
-};
+breedsSelect.addEventListener('change', onSelect);
 
-breeds.addEventListener('change', onSelect);
+let breedsList = [];
+
+fetchBreeds()
+  .then(data => {
+    data.map(breed => {
+      breedsList.push({ text: breed.name, value: breed.id });
+      renderSelectBreeds();
+    });
+  })
+  .catch(err => console.log(err));
+
+function onSelect(e) {
+  const breedId = e.target.value;
+}
+
+function renderSelectBreeds() {
+  breedsList.forEach(breed => {
+    const option = document.createElement('option');
+    option.textContent = breed.text;
+    option.value = breed.id;
+    breedsSelect.appendChild(option);
+  });
+}
